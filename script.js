08f46aa75d4473560d320717b0ba75ddd25ae000
Liana <smartghil4@gmail.com>
2024-09-04 15:55:44 +0300

    project

--- script.js
+++ script.js
@@ -36,5 +36,51 @@ window.onload = function (){
        const form = document.getElementById('form')
        form.appendChild(section)
       }
+
+    
+         function createInputs (){
+          const inputs = [
+            { class: 'name', type: 'text', name: 'order-name', id: 'order-name', placeholder: 'User name' },
+            { class: 'email', type: 'text', name: 'order-email', id: 'order-email', placeholder: 'Email' },
+            { class: 'pay', type: 'text', name: 'order-pay', id: 'order-pay', placeholder: 'Payment method' }
+        ];
+        return inputs;
+         }
       
+        
+        function createForm () {
+          const isExist = document.getElementById('form1')
+          if (isExist){
+            return;
+          }
+          const section = document.createElement('section');
+          section.className = 'data';
+          section.id = 'form1'
+          const innerDiv = document.createElement('div');
+          
+          const inputs = createInputs();
+  
+          inputs.forEach(input => {
+              const div = document.createElement('div');
+              const inputElement = document.createElement('input');
+            
+              inputElement.className = input.class;
+              inputElement.type = input.type;
+              inputElement.name = input.name;
+              inputElement.id = input.id;
+              inputElement.placeholder = input.placeholder;
+  
+              div.appendChild(inputElement);
+              innerDiv.appendChild(div);
+          });
+  
+          section.appendChild(innerDiv);
+  
+          document.getElementById('form-container').appendChild(section);
+    
+
+        }
+
+        const uiButton =  document.getElementById('ui');
+        uiButton.addEventListener('click', createForm)
 }
